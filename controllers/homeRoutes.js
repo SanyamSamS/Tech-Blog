282767906745b29e121c4e0565b1742ce49f7931
SanyamSamS <sanyam.edu@gmail.com>
2024-02-08 18:28:30 -0500

    Added routing and views for edit, login and dasahboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -62,5 +62,34 @@ router.get('/post/:id', async (req, res) => {
   }
 });
 
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+  res.render('login');
+});
+
+router.get('/dashboard', withAuth, async (req, res) =>{
+  try {
+    const postData = await Post.findAll({where:{userId: req.session.user_id}})
+    const posts = postData.map(post => post.get({ plain: true }))
+    res.render("dashboard", {posts, logged_in: req.session.logged_in })
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+router.get('/edit/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id)
+    const post = postData.get({ plain: true })
+    res.render('edit', {post, logged_in: req.session.logged_in})
+
+  } catch (error) {
+    res.status(500).json(err);
+  }
+})
 
 module.exports = router;
--- models/Post.js
+++ models/Post.js
@@ -26,6 +26,11 @@ Post.init(
         key: 'id',
       },
     },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
   },
   {
     sequelize,
