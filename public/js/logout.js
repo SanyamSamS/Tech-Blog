3a4c7702ec7dc0b28cfd2ead6bc1b7796c5aa5e7
SanyamSamS <sanyam.edu@gmail.com>
2024-02-24 04:32:52 -0500

    added an idle timer

--- public/js/logout.js
+++ public/js/logout.js
@@ -12,3 +12,23 @@ const logout = async () => {
 };
 
 document.querySelector('#logout').addEventListener('click', logout);
+
+// Idle timer
+let idleTime = 0;
+const idleInterval = setInterval(timerIncrement, 60000); // 1 minute
+
+function resetIdleTimer() {
+    idleTime = 0;
+}
+
+window.onload = resetIdleTimer;
+document.onmousemove = resetIdleTimer;
+document.onkeydown = resetIdleTimer;
+
+function timerIncrement() {
+  idleTime = idleTime + 1;
+
+  if (idleTime > 5) { // 5 minutes of inactivity
+      logout();
+  }
+}
